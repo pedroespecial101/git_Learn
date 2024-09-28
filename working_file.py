e2e99f600659acd0c5cabfbba5167cee57d553f1
Pete Treadaway <pedroespecial101@gmail.com>
2024-09-28 06:20:21 +0000

    fucked the code - doh!

--- working_file.py
+++ working_file.py
@@ -4,3 +4,7 @@ I'm so very pleased that it works and want to save this away.
 2) Now I'm adding some stuff and yey! it still works
 
 3) I think I will stash this because I want to be sure and I will commit also.
+
+Looks like maybe a 'stash' is an interim measure??
+
+Oh no!!!!  I've just FUCKED The Code and saved it and all!!!!
